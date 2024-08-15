9d9007ba41b348a38502031ee3d2740bbb3bb35d
pf274 <peterfullmer33@gmail.com>
2024-08-15 16:02:00 -0600

    Debounce searching

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import {solve} from './solver';
 import SideEntry from './components/SideEntry';
-import { List, ListItem, Table, TableBody, TableCell, TableRow, TextField } from '@mui/material';
+import { Button, List, ListItem, Table, TableBody, TableCell, TableRow, TextField } from '@mui/material';
 
 function App() {
   const sides = useRef<string[]>(['', '', '', '']);
@@ -13,6 +13,21 @@ function App() {
   const [solutionIndex, setSolutionIndex] = useState(0);
   const [wordsToExclude, setWordsToExclude] = useState('');
   const [wordsToInclude, setWordsToInclude] = useState('');
+  const [filteredSolutions, setFilteredSolutions] = useState<string[][]>([]);
+  const debounce = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (debounce.current) {
+      clearTimeout(debounce.current);
+    }
+    debounce.current = setTimeout(() => {
+      if (solutions.length > 0) {
+        setFilteredSolutions(filterSolutions());
+      } else {
+        setFilteredSolutions([]);
+      }
+    }, 1000);
+  }, [solutions, wordsToExclude, wordsToInclude]);
 
   function handleUpdateExclude(event: React.ChangeEvent<HTMLInputElement>) {
     const newWordsToExclude = event.target.value.toLowerCase().replace(/[^a-z ]/g, '');
@@ -26,9 +41,23 @@ function App() {
     setSolutionIndex(0);
   }
 
-  function getSolutions() {
+  function filterSolutions() {
     const notExcluded = solutions.filter((solution) => !solution.some((word) => wordsToExclude.split(' ').filter((segment) => segment.length > 0).includes(word)));
     return notExcluded.filter((solution) => wordsToInclude.split(' ').filter((segment) => segment.length > 0).every((word) => solution.includes(word)));
+  }
+
+  function getMostEfficientSolution() {
+    if (filteredSolutions.length == 0) {
+      return [];
+    }
+    return filteredSolutions.sort((a, b) => a.join('').length - b.join('').length)[0];
+  }
+
+  function getMostElaborateSolution() {
+    if (filteredSolutions.length == 0) {
+      return [];
+    }
+    return filteredSolutions.sort((a, b) => b.join('').length - a.join('').length)[0];
   }
 
   useEffect(() => {
@@ -102,23 +131,28 @@ function App() {
           </TableBody>
         </Table>
       </div>
-      <button onClick={runSolver}>Solve</button>
+      <Button variant="contained" onClick={runSolver} disabled={loading}>Solve</Button>
 
       {!loading && !hasChanged && <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1em'}}>
         <h2 style={{margin: 0, marginTop: '1em'}}>Solutions:</h2>
           <TextField label="Words to exclude" value={wordsToExclude} onChange={handleUpdateExclude}></TextField>
           <TextField label="Words to include" value={wordsToInclude} onChange={handleUpdateInclude}></TextField>
+          <h3 style={{margin: 0, padding: 0}}>Most efficient solution:</h3>
+          <p style={{margin: 0, padding: 0}}>{getMostEfficientSolution().join(', ')}</p>
+          <h3 style={{margin: 0, padding: 0}}>Most elaborate solution:</h3>
+          <p style={{margin: 0, padding: 0}}>{getMostElaborateSolution().join(', ')}</p>
+          <h3 style={{margin: 0, padding: 0}}>All Solutions:</h3>
           <List dense={true} style={{backgroundColor: '#eeeeee', borderRadius: '1em'}}>
-            {getSolutions().slice(solutionIndex, solutionIndex + 100).map((solution, index) => (
+            {filteredSolutions.slice(solutionIndex, solutionIndex + 100).map((solution, index) => (
               <ListItem key={index}>{solution.join(', ')}</ListItem>
             ))}
           </List>
-          {getSolutions().length == 0 && <p>No solutions found.</p>}
-          {getSolutions().length != 0 && getSolutions().slice(solutionIndex, solutionIndex + 100).length == 0 && <p>There are no more solutions.</p>}
+          {filteredSolutions.length == 0 && <p>No solutions found.</p>}
+          {filteredSolutions.length != 0 && filteredSolutions.slice(solutionIndex, solutionIndex + 100).length == 0 && <p>There are no more solutions.</p>}
           <div style={{display: 'flex', flexDirection: 'row', gap: '1em'}}>
             {solutionIndex > 0 && <button onClick={() => setSolutionIndex(Math.max(0, solutionIndex - 100))}>Go Back</button>}
-            <p>{Math.floor(solutionIndex / 100) + 1}/{Math.ceil(getSolutions().length / 100)}</p>
-            {solutionIndex < getSolutions().length && <button onClick={() => setSolutionIndex(Math.min(getSolutions().length, solutionIndex + 100))}>See more</button>}
+            <p>{Math.floor(solutionIndex / 100) + 1}/{Math.ceil(filteredSolutions.length / 100)}</p>
+            {solutionIndex < filteredSolutions.length && <button onClick={() => setSolutionIndex(Math.min(filteredSolutions.length, solutionIndex + 100))}>See more</button>}
           </div>
       </div>}
       {loading && <div>Loading...</div>}
